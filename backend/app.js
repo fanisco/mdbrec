c934bdbd1ea22de937f16146a1775d13913c9df3
Fanil Zubairov <fzubairov@xiag.ch>
2025-06-25 16:47:01 +0200

    delete movies api

--- backend/app.js
+++ backend/app.js
@@ -3,14 +3,12 @@ import dotenv from "dotenv";
 import cors from "cors";
 dotenv.config();
 
-import moviesRouter from "./routes/movies.js";
 import recommendRouter from "./routes/recommend.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
-app.use("/movies", moviesRouter);
 app.use("/api/recommend", recommendRouter);
 
 export default app;
--- backend/routes/movies.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getMovies } from "../controllers/moviesController.js";
-
-const router = express.Router();
-
-router.get("/", getMovies);
-
-export default router;
